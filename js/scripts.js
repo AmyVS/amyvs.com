ec1d75b98d9c10d2b33227ce77daa28beb38abc1
amyvs <amydecat@yahoo.com>
2014-12-07 22:54:41 -0800

    add a div for content that fades in, if menu item is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var change_nav_height = 8;
 var original_nav_top = 19;
 
 $(document).ready(function(){
+  $(".content").hide();
 
   $(".menu-item").click(function(){
     $(".main-nav").slideUp("slow", "swing", function(){
@@ -10,6 +11,7 @@ $(document).ready(function(){
       $(".main-nav").css("display", "block");
       $(".main-nav").css("max-height", change_nav_height+"px");
     });
+    $(".content:hidden:first").fadeIn("slow");
   });
 
   $("#name").click(function() {
