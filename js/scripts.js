ce63609a6d299961b7ec87e04a38cc4d07dd7433
amyvs <amydecat@yahoo.com>
2014-12-07 22:22:58 -0800

    restore page to original setting when my brand name is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
-var main_nav_top = -8;
-var main_nav_height = 8;
+var change_nav_top = -8;
+var change_nav_height = 8;
+var original_nav_top = 19;
 
 $(document).ready(function(){
 
   $(".menu-item").click(function(){
     $(".main-nav").slideUp("slow", "swing", function(){
-      $(".main-nav").css("margin-top", main_nav_top+"px");
+      $(".main-nav").css("margin-top", change_nav_top+"px");
       $(".main-nav").css("display", "block");
-      $(".main-nav").css("max-height", main_nav_height+"px");
+      $(".main-nav").css("max-height", change_nav_height+"px");
     });
   });
 
+  $("#name").click(function() {
+    location.reload();
+  });
 });
